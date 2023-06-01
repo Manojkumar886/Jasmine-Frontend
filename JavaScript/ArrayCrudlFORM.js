d0280701814ddaaaaefa398639b469eea6369c36
Manojkumar886 <maddymano434@gmail.com>
2023-06-01 15:21:09 +0530

    Array Crudl is done

--- JavaScript/ArrayCrudlFORM.js
+++ JavaScript/ArrayCrudlFORM.js
@@ -5,6 +5,7 @@ let Adding=()=>
     let newchoconame=document.getElementById('uservalue').value
     ChocolateNames.unshift(newchoconame)
     alert(newchoconame+"your chocolate name is added successfully")
+    Listing()
 }
 
 var Listing=()=>
@@ -16,4 +17,39 @@ var Listing=()=>
     })
     document.getElementById('live').innerHTML="<table align='center' style='box-shadow:5px 7px 4px gray'><thead><tr><th>Chocolatename</th></tr></thead><tbody>"+choco1+"</tbody></table>"
 
+}
+
+let Updating=()=>
+{
+    let index=document.getElementById('uservalue').value
+
+    // var newvalue=prompt("please tell us which value you want update/replace"+ChocolateNames[index])
+    const newvalue="Something"
+    ChocolateNames[index]=newvalue;
+    Listing()
+}
+
+var Removing=()=>
+{
+    let deletedvalue=document.getElementById('uservalue').value
+    alert(ChocolateNames[deletedvalue]+' has been deleted successfully');
+    ChocolateNames=ChocolateNames.filter((data)=>
+    { 
+        return data!==ChocolateNames[deletedvalue]
+    })
+    
+    Listing()
+}
+
+
+let Searching=()=>
+{
+    let temp=document.getElementById('uservalue').value
+    for(let pos=0;pos<ChocolateNames.length;pos++)
+    {
+        if(ChocolateNames[pos]===temp)
+        {
+            return pos;
+        }
+    }
 }
